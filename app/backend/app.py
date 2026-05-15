3b97d3f0e3fbabca4786c3e85c8d9c5402a54dfb
Shubhamjadhavrao <28shubhamjadhav@gmail.com>
2026-05-15 11:54:21 +0530

    done

--- app/backend/app.py
+++ app/backend/app.py
@@ -93,8 +93,9 @@ def add_cart():
     conn.commit()
     conn.close()
     return jsonify({'message': 'Added'}), 201
-
-if __name__ == '__main__':
+with app.app_context():
     init_db()
+    
+if __name__ == '__main__':
     app.run(host='0.0.0.0',
             port=5000, debug=True)
